b4e3cbbba7e68ec03581255b421dee871528d99b
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-04-20 21:55:36 -0300

    delete task

--- app.js
+++ app.js
@@ -12,27 +12,55 @@ form.addEventListener('submit', function(event){
   const newArticle = document.createElement('article')
   newArticle.setAttribute('data-id' , id)
   newArticle.classList.add('todo-list')
-  newArticle.innerHTML =
-  // img check 2 
-  // <button class="btn-check btn">
-  //   <i class="fa-solid fa-circle-check"></i>
-  // </button>
-  ` <div class="todo-check">
-      <button class="btn-check btn">
-        <i class="fa-solid fa-circle"></i>
-      </button>
-      <p class="todo-title">${value}</p>
-    </div>
 
-    <div class="todo-btn">
-      <button class="edit btn-control">
-        <i class="fa-solid fa-pen-to-square"></i>
-      </button>
-      <button class="delete btn-control">
-        <i class="fa-solid fa-trash"></i>
-      </button>
-    </div>`
+    if(value != ''){
+      newArticle.innerHTML =
+    // img check 2 
+    // <button class="btn-check btn">
+    //   <i class="fa-solid fa-circle-check"></i>
+    // </button>
+    `<div class="todo-check">
+        <button class="btn-check btn">
+          <i class="fa-solid fa-circle"></i>
+        </button>
+        <p class="todo-title">${value}</p>
+      </div>
 
-  container.appendChild(newArticle)
-  console.log('Criado')    
+      <div class="todo-btn">
+        <button class="edit btn-control">
+          <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button class="delete btn-control">
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </div>`
+
+    container.appendChild(newArticle)
+    setBacktoDefault()
+
+    const deleteBtn = document.querySelector('.delete')
+    const editBtn = document.querySelector('.edit')
+    deleteBtn.addEventListener('click', deleteItem)
+    editBtn.addEventListener('click', editItem)
+    }
+    else{
+      console.log('Erro inesperado!')
+    }
 })
+
+
+function deleteItem(e){
+  const item = e.currentTarget.parentElement.parentElement
+  const id = item.dataset.id
+
+  container.removeChild(item)
+  console.log('deletando!')
+}
+
+function editItem(e){
+  console.log('editandp')
+}
+
+function setBacktoDefault(){
+  inputForm.value = ''
+}
