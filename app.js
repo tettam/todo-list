df7a411ddccb3f1f3412d7609c57e93758e4dd17
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-04-21 16:05:46 -0300

    local storage

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const appearance = document.querySelector('.icon-config')
 const sectionBottom = document.querySelector('.section-bottom')
 
 //edit option
-let editElement;
+let editElement
 let editFlag = false
+let theme = 'white'
+let editId = ''
 
 appearance.addEventListener('click', appearanceTheme)
 form.addEventListener('submit', addItem)
+window.addEventListener('DOMContentLoaded', setupItems)
 
 
 function addItem(event){
@@ -46,17 +49,22 @@ function addItem(event){
     deleteBtn.addEventListener('click', deleteItem)
     const editBtn = newArticle.querySelector('.edit')
     editBtn.addEventListener('click', editItem)
-
     //append chield
     container.appendChild(newArticle)
+    
+    appearanceThemeCreatedItem(newArticle)
     setBacktoDefault()
     checkEvent(newArticle, 'success')
+    // local storage
+    addToLocalStorage(id, value)
+  
     }
     else if(value != '' && editFlag){
       const item = editElement.querySelector('.todo-title')
       const element = item.parentElement.parentElement
       item.innerHTML = value
       setBacktoDefault()
+      editLocalStorage(editId , value)
       checkEvent(element , 'edit')
     } 
     else {
@@ -79,14 +87,18 @@ function deleteItem(e){
   const item = e.currentTarget.parentElement.parentElement
   const id = item.dataset.id
 
+  removeLocalStorage(id)
   container.removeChild(item)  
 }
 
 function editItem(e){
   const element = e.currentTarget.parentElement.parentElement
+  const id = element.dataset.id
+
   editElement = e.currentTarget.parentElement.previousElementSibling
   inputForm.value = editElement.innerText
   editFlag = true
+  editId = id
 }
 
 function checkEvent(e, option){
@@ -116,6 +128,7 @@ function appearanceTheme(e){
     // section bottom
     sectionBottom.classList.add('appearance-section')
     // list(todo list)
+    theme = 'dark'
     containerItems.forEach((element) => {
       element.classList.add(APPEARANCE_WRITE)
     })
@@ -124,8 +137,93 @@ function appearanceTheme(e){
     item.classList.replace('fa-moon', 'fa-sun')
     form.classList.remove(APPEARANCE_WRITE)
     sectionBottom.classList.remove('appearance-section')
+    theme = 'write'
     containerItems.forEach((element) => {
       element.classList.remove(APPEARANCE_WRITE)
     })
   }
+}
+
+function appearanceThemeCreatedItem(e){
+  if(theme == 'dark'){
+    e.classList.add('appearance-white')
+  }
+   
+}
+
+/* local storage */
+function getLocalStorage(){
+  if(localStorage.getItem('list')){
+    return JSON.parse(localStorage.getItem('list'))
+  } 
+  return []
+}
+// add local storage
+function addToLocalStorage(id, value){
+  const toDo = {id , value}
+  let items = getLocalStorage()
+  items.push(toDo)
+  localStorage.setItem('list', JSON.stringify(items))
+}
+// remove local storage
+function removeLocalStorage(id){
+  let items = getLocalStorage()
+  items = items.filter(function(item){
+    if(item.id !== id){
+      return item
+    }
+  })
+
+  localStorage.setItem('list', JSON.stringify(items))
+}
+// edit local storage
+function editLocalStorage(id, value){
+  let items = getLocalStorage()
+  items = items.map(function(item){
+    if(item.id == id){
+      item.value = value
+    }
+    return item
+  })
+  localStorage.setItem('list', JSON.stringify(items))
+}
+
+function setupItems(){
+  let items = getLocalStorage()
+  if(items.length > 0){
+    items.forEach(function(item){
+      createListItem(item.id, item.value)
+    })
+  }
+}
+
+// list items local storage
+function createListItem(id, value){
+  let newArticle = document.createElement('article')
+  newArticle.setAttribute('data-id', id)
+  newArticle.classList.add('todo-list')
+
+  newArticle.innerHTML = `<div class="todo-check">
+                          <button class="btn-check btn">
+                            <i class="fa-solid fa-circle"></i>
+                          </button>
+                          <p class="todo-title">${value}</p>
+                        </div>
+                        <div class="todo-btn">
+                          <button class="edit btn-control">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                          </button>
+                          <button class="delete btn-control">
+                            <i class="fa-solid fa-trash"></i>
+                          </button>
+                        </div>`
+
+  const checkBtn = newArticle.querySelector('.btn-check')
+  checkBtn.addEventListener('click', toogleCheckIcon)
+  const deleteBtn = newArticle.querySelector('.delete')
+  deleteBtn.addEventListener('click', deleteItem)
+  const editBtn = newArticle.querySelector('.edit')
+  editBtn.addEventListener('click', editItem)
+  //append chield
+  container.appendChild(newArticle)
 }
